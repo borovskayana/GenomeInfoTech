735fdb5e7eb49df497c3e92a125dd4f5d9b16fc4
borovskayana <borovskayana7@gmail.com>
2023-08-16 11:23:08 +0300

    Update

--- src/components/FooterElement.js
+++ src/components/FooterElement.js
@@ -1,10 +1,73 @@
 import { Layout } from "antd";
 import { Typography } from "antd";
-const { Title } = Typography;
+import { Col, Row } from 'antd';
+import { useNavigate } from "react-router-dom";
+import { Menu } from "antd";
 
+const { Title } = Typography;
 const { Footer } = Layout;
 
+
+
+const style = {
+  color: "white",
+  alignItems: "center"
+  
+};
+
 function FooterElement() {
+  const navigate = useNavigate();
+  const items = [
+    {
+      label: "ABOUT US",
+      key: "/",
+    },
+    {
+      label: "PROJECTS",
+      key: "/projects",
+    },
+    {
+      label: "NEWS",
+      key: "/news",
+    },
+    {
+      label: "CONTACT US",
+      key: "/contact",
+    },
+  ];
+  const conditions = [
+    {
+      label: "TERMS & CONDITIONS",
+      key: "/terms",
+    },
+    {
+      label: "PRIVACY POLICY",
+      key: "/privacy",
+    },
+    {
+      label: "COOKIE POLICY",
+      key: "/policy",
+    },
+    {
+      label: "COOKIE SETTINGS",
+      key: "/settings",
+    },
+  ];
+  const socials = [
+    {
+      label: "FACEBOOK",
+      key: "facebook",
+    },
+    {
+      label: "LINKEDIN",
+      key: "linkedin",
+    },
+    {
+      label: "INSTAGRAM",
+      key: "instagram",
+    }
+  ];
+
   return (
     <div>
       <Footer
@@ -13,9 +76,56 @@ function FooterElement() {
  
         }}
       >
-        <Title style={{ color: "white" }} level={2}>
+       
+        <Row gutter={16}>
+      <Col className="gutter-row" span={9}>
+        <div style={style}>    <Title style={{ color: "white" }} level={2}>
           Genome InfoTech
-        </Title>
+        </Title></div>
+      </Col>
+      <Col className="gutter-row" span={4}>
+            <div style={style}>
+            <Menu
+          mode="vertical"
+          items={items}
+          onClick={({ key }) => navigate(key)}
+          style={{
+    
+            background: "rgba(0, 0, 0, .0)",
+            color: "white",
+            alignItems: "end",
+          }}
+        />
+            </div>
+      </Col>
+      <Col className="gutter-row" span={5}>
+        <div style={style}>  <Menu
+          mode="vertical"
+          items={conditions}
+          onClick={({ key }) => navigate(key)}
+          style={{
+    
+            background: "rgba(0, 0, 0, .0)",
+            color: "white",
+            alignItems: "end",
+          }}
+        /></div>
+      </Col>
+      <Col className="gutter-row" span={4}>
+        <div style={style}> <Menu
+          mode="vertical"
+          items={socials}
+          onClick={({ key }) =>  window.location.href = `https://www.${key}.com/`}
+          style={{
+    
+            background: "rgba(0, 0, 0, .0)",
+            color: "white",
+            alignItems: "end",
+          }}
+        /></div>
+      </Col>
+
+    </Row>
       </Footer>
     </div>
   );
