c2a98193e5a8889b93787a97b423194d64f953b8
borovskayana <borovskayana7@gmail.com>
2023-09-04 08:38:18 +0300

    Update

--- src/components/sign-in-components/PersonalCabinet.js
+++ src/components/sign-in-components/PersonalCabinet.js
@@ -2,8 +2,8 @@ import "../../css/sign-in/PersonalCabinet.css";
 
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { Button, Space } from "antd";
-import { SettingOutlined } from "@ant-design/icons";
+import { Button, Form, Input, Modal } from "antd";
+import { SettingOutlined, EditOutlined } from "@ant-design/icons";
 
 import { Layout, Typography, Row, Drawer } from "antd";
 
@@ -11,6 +11,16 @@ const { Content } = Layout;
 const { Title } = Typography;
 
 function PersonalCabinet() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   const [open, setOpen] = useState(false);
   const showDrawer = () => {
     setOpen(true);
@@ -23,19 +33,64 @@ function PersonalCabinet() {
     localStorage.clear();
     navigate("/");
   };
+
   const nickname = localStorage.getItem("nickname");
+  const [changes] = Form.useForm();
+
+  function onFinish(values) {
+    console.log(values.nicknamechange);
+    localStorage.setItem("nickname", values.nicknamechange);
+  }
+
   return (
     <>
       <Content className="content-cabinet">
-        <div style={{ textAlign: "right"}}>
-          <SettingOutlined
-            onClick={showDrawer}
-            style={{ fontSize: "2em"}}
-          />
+        <div style={{ textAlign: "right" }}>
+          <SettingOutlined onClick={showDrawer} style={{ fontSize: "2em" }} />
         </div>
         <Row justify="center">
           <Title level={3}>User {nickname}</Title>{" "}
+          <EditOutlined onClick={showModal} />
         </Row>
+
+        <Modal
+          title="Change Data"
+          open={isModalOpen}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          footer={null}
+        >
+          <Form
+            form={changes}
+            name="changes"
+            onFinish={onFinish}
+            scrollToFirstError
+          >
+            <Form.Item
+              name="nicknamechange"
+              label="Nickname"
+              tooltip="What do you want others to call you?"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your nickname!",
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                onClick={() => window.location.reload()}
+              >
+                Save changes
+              </Button>
+            </Form.Item>
+          </Form>
+        </Modal>
 
         <Drawer
           title="Settings"
